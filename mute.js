0704e6d911723acb657ef9cb3e0ce2fde2dcc1f0
brandonbee <brando.b@gmail.com>
2023-05-01 19:39:02 -0400

    Update mute.js

--- mute.js
+++ mute.js
@@ -12,7 +12,7 @@
 			if(isTweet){
 				tweetIndex = 1;
 			}
-			if(isTweet){
+			if(isHome||isTweet){
 				for(i=tweetIndex;i<tweets.length;i++){
 					var parent = tweets[i].parentNode.parentNode.parentNode;
 					var verified = parent.querySelector('[aria-label="Verified account"]') != null;
@@ -24,10 +24,7 @@
 						setTimeout(function(){
 							var nodeIndex = 2;
 							var dropdown = document.querySelectorAll('[data-testid="Dropdown"]');
-							//Ads have a different menu item placement, so move selection accordingly.
-							var isAd = dropdown[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0].innerText == "Not interested in this ad";
-							if(isAd){nodeIndex = 4;}
-							var muteButton = dropdown[0].childNodes[nodeIndex];
+							var muteButton = dropdown[0].querySelector('[d="M18 6.59V1.2L8.71 7H5.5C4.12 7 3 8.12 3 9.5v5C3 15.88 4.12 17 5.5 17h2.09l-2.3 2.29 1.42 1.42 15.5-15.5-1.42-1.42L18 6.59zm-8 8V8.55l6-3.75v3.79l-6 6zM5 9.5c0-.28.22-.5.5-.5H8v6H5.5c-.28 0-.5-.22-.5-.5v-5zm6.5 9.24l1.45-1.45L16 19.2V14l2 .02v8.78l-6.5-4.06z"]').parentNode.parentNode.parentNode.parentNode;
 							muteButton.click();
 						}, 750);
 					}
